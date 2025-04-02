1f06bfff0d31e46687ec434dced644c61634b855
Willman Giraldo Zambrano <143544629+WillmanGZ@users.noreply.github.com>
2025-04-01 20:54:01 -0500

    Añadir componente de formulario de inicio de sesión

---
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [],
+  templateUrl: './login-form.component.html',
+})
+export class LoginFormComponent { }

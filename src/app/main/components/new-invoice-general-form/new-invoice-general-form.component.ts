e7c6d7e0d34cfba480e7aee5553c7a6af50af9a9
WillmanGZ <willmang@uninorte.edu.co>
2025-04-18 14:57:55 -0500

    Metodos de pago

--- src/app/main/components/new-invoice-general-form/new-invoice-general-form.component.ts
+++ src/app/main/components/new-invoice-general-form/new-invoice-general-form.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NumberingRange } from '@main/models/numbering-ranges.model';
 import { NewInvoiceService } from '@main/services/new-invoice.service';
+import { PAYMENT_METHODS } from '@main/static/new-invoice.info';
 
 @Component({
   selector: 'app-new-invoice-general-form',
@@ -12,7 +13,10 @@ export class NewInvoiceGeneralFormComponent {
   private newInvoiceService = inject(NewInvoiceService);
 
   numberingRanges = signal<NumberingRange[]>([]);
+  readonly PAYMENT_METHODS = PAYMENT_METHODS;
+
   numbering_range_id = signal(8);
+  payment_method_id = signal(10);
 
   constructor() {
     this.getNumberingRanges();
---
+++ src/app/main/static/new-invoice.info.ts
@@ -0,0 +1,11 @@
+export const PAYMENT_METHODS = [
+  { code: 10, name: 'Efectivo' },
+  { code: 42, name: 'Consignación' },
+  { code: 20, name: 'Cheque' },
+  { code: 47, name: 'Transferencia' },
+  { code: 71, name: 'Bonos' },
+  { code: 72, name: 'Vales' },
+  { code: 1, name: 'Medio de pago no definido' },
+  { code: 49, name: 'Tarjeta Débito' },
+  { code: 48, name: 'Tarjeta Crédito' },
+];

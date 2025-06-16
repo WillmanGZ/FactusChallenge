ffb0268eba887bf30a24ca1607e68c257e2da066
WillmanGZ <willmang@uninorte.edu.co>
2025-06-16 14:45:40 -0500

    Refactor: new invoice form to use reactive forms

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
@@ -12,7 +12,7 @@ import { CookieService } from 'ngx-cookie-service';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withFetch()),
     provideAnimations(),
     provideToastr({
--- src/app/main/components/new-invoice-general-form/new-invoice-general-form.component.ts
+++ src/app/main/components/new-invoice-general-form/new-invoice-general-form.component.ts
@@ -1,26 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NumberingRange } from '@main/models/numbering-ranges.model';
 import { NewInvoiceService } from '@main/services/new-invoice.service';
 import { PAYMENT_METHODS } from '@main/static/new-invoice.info';
 
 @Component({
   selector: 'app-new-invoice-general-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './new-invoice-general-form.component.html',
 })
 export class NewInvoiceGeneralFormComponent {
-  private newInvoiceService = inject(NewInvoiceService);
-
-  numberingRanges = signal<NumberingRange[]>([]);
-  readonly PAYMENT_METHODS = PAYMENT_METHODS;
-
-  numbering_range_id = signal(8);
-  payment_method_id = signal(10);
-
   constructor() {
     this.getNumberingRanges();
   }
+
+  private newInvoiceService = inject(NewInvoiceService);
+  private fb = inject(FormBuilder);
+  readonly PAYMENT_METHODS = PAYMENT_METHODS;
+
+  numberingRanges = signal<NumberingRange[]>([]);
+  numbering_range_id = signal(8);
+  payment_method_id = signal(10);
+
+  myForm: FormGroup = this.fb.group({
+    numberingRange: [this.numbering_range_id(), Validators.required],
+    paymentMethod: [this.payment_method_id(), Validators.required],
+  });
 
   getNumberingRanges() {
     this.newInvoiceService.getNumberingRanges().subscribe({

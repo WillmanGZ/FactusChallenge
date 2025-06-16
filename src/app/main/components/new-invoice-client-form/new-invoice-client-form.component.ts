44cf23b081c130fbb16de095a8055d1fe62ddd95
WillmanGZ <willmang@uninorte.edu.co>
2025-06-16 18:26:21 -0500

    Refactor: update new invoice client form to use reactive forms and improve validation

--- src/app/main/components/new-invoice-client-form/new-invoice-client-form.component.ts
+++ src/app/main/components/new-invoice-client-form/new-invoice-client-form.component.ts
@@ -1,20 +1,43 @@
-import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { DOCUMENTS_TYPES } from '@main/static/new-invoice.info';
 
 @Component({
   selector: 'app-new-invoice-client-form',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './new-invoice-client-form.component.html',
 })
 export class NewInvoiceClientFormComponent {
+  private fb = inject(FormBuilder);
   readonly documentTypes = DOCUMENTS_TYPES;
 
   document_type_id = signal(3);
-  identification = signal('');
+  identification = signal<number | null>(null);
   dv = signal<number | null>(null);
   name = signal('');
   address = signal('');
   email = signal('');
   tel = signal('');
+
+  myForm: FormGroup = this.fb.group({
+    documentType: [this.document_type_id(), Validators.required],
+    identification: [
+      this.identification(),
+      [Validators.required, Validators.pattern('^\\d{6,10}$')],
+    ],
+    dv: [this.dv(), [Validators.required, Validators.pattern('^\\d$')]],
+    name: [this.name(), Validators.minLength(3)],
+    address: [this.address(), Validators.minLength(4)],
+    email: [
+      this.email(),
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'),
+    ],
+    tel: [this.tel(), Validators.pattern('^\\+?[0-9\\s\\-().]{7,20}$')],
+  });
 }
